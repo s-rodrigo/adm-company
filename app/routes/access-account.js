0a57ed443bfeda837994e9a193fb623934b3b614
Rodrigo Garcia dos Santos <rodrigo.santos@grupoinvestiga.com.br>
2019-02-27 14:18:08 -0300

    config heroku

--- app/routes/access-account.js
+++ app/routes/access-account.js
@@ -1,7 +1,7 @@
 module.exports = application => {
     application.post('/public/login', (req, res) => {
-        console.log('route');
         console.log(application.controllers);
+        console.log(application.models);
         application.controllers.AccessAccountCtrl.Login(application, req, res);
     });
 }
