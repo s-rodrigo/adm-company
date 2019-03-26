bb1caf7fbf2ae0fa5307729d90dbdb92249171eb
Rodrigo Garcia dos Santos <rodrigo.santos@grupoinvestiga.com.br>
2019-03-26 12:05:37 -0300

    heroku

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const MongoClient = mongodb.MongoClient;
 const ObjectId = mongodb.ObjectID;
  
 // Connection URL
-const user = 'rodrigogsantos';
-const password = '021209Fe';
+const user = 'adminrod';
+const password = '110893rod';
 const url = 'mongodb://'+user+':'+password+'@ds047440.mlab.com:47440/company-jobs';
  
 // Database Name
